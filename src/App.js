b8937c0394b85c22221274871d1c644468e93a3b
Nikhil <nikhilgupta.post@gmail.com>
2024-07-05 02:45:59 +0530

    Product details

--- src/App.js
+++ src/App.js
@@ -22,42 +22,12 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
-          <Route
-            path="/green"
-            element={[<Headergreen />, <Navbarg />, <Homegreen />, <Footer />]}
-          />
-          <Route
-            path="/"
-            element={[<Header />, <NavBar />, <Home />, <Footer />]}
-          />
-          <Route
-            path="/seller"
-            element={[
-              <Headergreen />,
-              <Navbarg />,
-              <SellerSection />,
-              <Footer />,
-            ]}
-          />
-          <Route
-            path="/sustainability"
-            element={[
-              <Headergreen />,
-              <Navbarg />,
-              <SustainabilityReportsSection />,
-              <Footer />,
-            ]}
-          />
+          <Route path="/green" element={[<Headergreen />, <Navbarg />, <Homegreen />, <Footer />]}/>
+          <Route path="/"element={[<Header />, <NavBar />, <Home />, <Footer />]}/>
+          <Route path="/seller" element={[<Headergreen />,<Navbarg />,<SellerSection />,<Footer />]}/>
+          <Route path="/sustainability" element={[<Headergreen />,<Navbarg />,<SustainabilityReportsSection />,<Footer />,]}/>
 
-          <Route
-            path="/education"
-            element={[
-              <Headergreen />,
-              <Navbarg />,
-              <EducationSection />,
-              <Footer />,
-            ]}
-          />
+          <Route path="/education" element={[<Headergreen />,<Navbarg />,<EducationSection />,<Footer />]}/>
           <Route path="/orders" element={[<Header />, <Navbarg />, <Orders/>]} />
           <Route path="/checkout" element={[<Header />, <Checkout/>, <Footer/>  ]}/>
           <Route path="/dashboard" element={[<Header />, <Navbarg />]} />
